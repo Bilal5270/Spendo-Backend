6b041f2031d7ae9e6e770d0ee24e01fe91ce31ee
Bilal Yousef <s1169284@student.windesheim.nl>
2025-04-09 13:45:17 +0200

    changed context

--- Spendo-Backend/Models/SpendoContext.cs
+++ Spendo-Backend/Models/SpendoContext.cs
@@ -25,7 +25,7 @@ public partial class SpendoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=SPENDO;Username=postgres;Password=DB2002");
+        => optionsBuilder.UseNpgsql("Host=gcoccw8ws4okgks0kg0s4oww;Database=SPENDO;Username=postgres;Password=DB2002");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
